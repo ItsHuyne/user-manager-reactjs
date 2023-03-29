61f348d68a8830914d208ac20e844e7d64f38af7
marioluckiluke <bdfnatsu@gmail.com>
2023-03-29 10:18:59 +0700

    Nhu cac

--- frontend/src/components/DataGrid.js
+++ frontend/src/components/DataGrid.js
@@ -1,48 +1,68 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import { DataGrid } from "@mui/x-data-grid";
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import { Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { axiosClient } from '../helper/helper';
+import { useAuthStore } from '../store/store';
+
+const isAdmin = sessionStorage.getItem('isAdmin');
+const userId = sessionStorage.getItem('userId');
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { field: 'id', headerName: 'ID', width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
+    field: 'firstName',
+    headerName: 'First name',
     width: 150,
-    editable: false,
+    editable: false
   },
   {
-    field: "lastName",
-    headerName: "Last name",
+    field: 'lastName',
+    headerName: 'Last name',
     width: 150,
-    editable: false,
+    editable: false
   },
   {
-    field: "adress",
-    headerName: "adress",
+    field: 'adress',
+    headerName: 'adress',
     width: 300,
-    editable: false,
+    editable: false
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
+    field: 'actions',
+    type: 'actions',
+    width: 80,
+    getActions: (params) => [
+      <GridActionsCellItem
+        key={params.id}
+        icon={<DeleteIcon />}
+        label="Delete"
+        showInMenu
+        disabled={isAdmin === 'false'}
+      />,
+      <GridActionsCellItem
+        key={params.id}
+        icon={<EditIcon />}
+        label="Update"
+        showInMenu
+        disabled={params.id !== userId}
+      />
+    ]
+  }
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
 ];
 
 const DataGridMem = () => {
@@ -51,16 +71,16 @@ const DataGridMem = () => {
   // floatingFilter: true,
   return (
     <div className="content bg-dark">
-      <Box sx={{ height: 400, width: "100%" }}>
+      <Box sx={{ height: 400, width: '100%' }}>
         <DataGrid
           rows={rows}
           columns={columns}
           initialState={{
             pagination: {
               paginationModel: {
-                pageSize: 5,
-              },
-            },
+                pageSize: 5
+              }
+            }
           }}
           pageSizeOptions={[5]}
           checkboxSelection
